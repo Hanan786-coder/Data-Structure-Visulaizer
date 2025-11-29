f5ab01094d375abefd48a2b8f44e5af4b830b16b
Hanan <hananbhatti412@gmail.com>
2025-11-29 17:01:13 +0500

    Added Colors.py and gitignore

---
+++ Colors.py
@@ -0,0 +1,7 @@
+ORANGE = (255, 108, 12) # Highlight
+GREY = (57, 62, 70) # Background
+TEAL = (0, 173, 181) # Nodes and Buttons
+TEAL_BRIGHT = (0, 200, 210) # Button Hover
+# Extras
+BLACK = (43, 40, 49)
+LIGHT_GREY = (238, 238, 238)
