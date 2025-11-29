6615981b587845e4aeb749a42c343d832158a3b8
Hanan <hananbhatti412@gmail.com>
2025-11-29 18:41:48 +0500

    Added general button class (can create custom buttons)

---
+++ button_template.py
@@ -0,0 +1,27 @@
+import pygame
+import Colors
+
+class Button:
+    def __init__(self, x, y, width, height, text, color, hover_color, text_size, text_color=Colors.WHITE):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.hover_color = hover_color
+        self.text = text
+        self.text_color = text_color
+        self.font = pygame.font.Font("ScienceGothic-Regular.ttf",text_size)
+
+    def draw(self, screen):
+        # Change color on hover
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, self.hover_color, self.rect)
+        else:
+            pygame.draw.rect(screen, self.color, self.rect)
+
+        # Write text
+        text_surf = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        screen.blit(text_surf, text_rect)
+
+    def is_clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
